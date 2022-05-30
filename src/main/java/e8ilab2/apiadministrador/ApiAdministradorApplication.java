b1c785c9df66b1aaf884ed1b3efe30d44af57a3b
jessica.zanchetta <jessica.zanchetta@MacBook-Pro.local>
2022-05-30 16:12:56 -0300

    first commit - project setup

---
+++ src/main/java/e8ilab2/apiadministrador/ApiAdministradorApplication.java
@@ -0,0 +1,13 @@
+package e8ilab2.apiadministrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdministradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdministradorApplication.class, args);
+	}
+
+}
